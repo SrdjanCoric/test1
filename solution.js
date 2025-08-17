4c68f595515d40302ff8b9250bd87a4fe04158bc
Srdjan Coric <srki.coric@gmail.com>
2025-08-17 10:20:57 +0200

    update

--- solution.js
+++ solution.js
@@ -1,9 +1,3 @@
 function contains(element, list) {
-  for (let i = 0; i < list.length; i++) {
-    if (list[i] === element) {
-      return true;
-    }
-  }
-
-  return false;
+  return list.some((item) => item === element);
 }
